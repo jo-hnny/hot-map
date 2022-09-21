57f68e5146c890ea24146f29b71c225530ff8867
johnny19941216 <wuzhiqiang94@hotmail.com>
2022-09-21 21:45:18 +0800

    feat: 优化了渲染逻辑

--- src/hot-map.ts
+++ src/hot-map.ts
@@ -3,7 +3,7 @@ import middleImg from "./assets/4-hov.svg";
 import highImg from "./assets/5-hov.svg";
 import lowImg from "./assets/1-hov.svg";
 import { randNumber, randText } from "@ngneat/falso";
-import _ from "lodash";
+import _, { size } from "lodash";
 import "./index.less";
 
 interface ISprite extends PIXI.Sprite {
@@ -17,37 +17,53 @@ function getParamByUrl(key: string) {
 
 const MaxItem = +(getParamByUrl("max") ?? 5) * 10000;
 
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
 let itemSize = 50;
 
-let minSpace = 1;
+const space = 20;
+
+// 竖向拖动
+let startY = 0;
+let offsetY = 0;
+let dragAble = false;
 
 function main() {
-  // console.log("is webgl", PIXI.utils.isWebGLSupported());
-  const windowWidth = window.innerWidth;
-  const windowHeight = window.innerHeight - 5;
-
-  const rowNum = Math.floor(windowWidth / (itemSize + minSpace));
-
-  const rowSpace = (windowWidth - rowNum * itemSize) / (rowNum - 1);
-
-  console.log(rowNum, rowSpace);
-
   const pop = document.querySelector(".pop") as HTMLDivElement;
 
   const app = new PIXI.Application({
-    width: windowWidth,
-    height: windowHeight,
+    resolution: devicePixelRatio,
+    antialias: true,
+    resizeTo: window,
   });
 
-  // const pc = new PIXI.ParticleContainer(MaxItem, {
-  //   scale: true,
-  // });
+  app.stage.interactive = true;
 
-  // app.stage.addChild(pc);
+  app.stage.hitArea = app.renderer.screen;
+
+  app.stage.addListener("mousemove", (e) => {
+    if (dragAble) return;
+
+    if (e?.target?.isSprite) {
+      const { clusterId, status } = e.target._customData;
+
+      pop.innerHTML = `clusterId: ${clusterId} \n status: ${status}`;
+      pop.className = `pop ${status}`;
+
+      pop.style.setProperty("top", `${e.target.y}px`);
+      pop.style.setProperty("left", `${e.target.x}px`);
+
+      pop.style.setProperty("display", "block");
+    } else {
+      pop.style.setProperty("display", "none");
+      pop.className = "pop";
+    }
+  });
 
   document.body.appendChild(app.view);
 
-  const svgSize = { resourceOptions: { scale: 2 }, width: 100, height: 100 };
+  const svgSize = { resourceOptions: { scale: 2 } };
 
   const textures = {
     low: PIXI.Texture.from(lowImg, svgSize),
@@ -56,91 +72,35 @@ function main() {
   };
 
   const dudes = [...new Array(MaxItem)].map((_, index) => {
-    const y = Math.floor(index / rowNum) * (itemSize + 5);
-
-    const x = (index % rowNum) * (itemSize + rowSpace);
-
     const status = ["low", "middle", "high"][randNumber({ min: 0, max: 2 })] as
       | "low"
       | "middle"
       | "high";
+
     const dude: ISprite = new PIXI.Sprite(textures[status]);
 
-    // dude.tint = Math.random() * 0xffffff;
+    dude.visible = false;
+
+    dude.interactive = false;
 
     dude._customData = {
       clusterId: randText(),
       status,
     };
 
-    dude.width = itemSize;
-    dude.height = itemSize;
-
-    dude.x = x;
-
-    dude.y = y;
-
-    dude.interactive = true;
-
-    if (y > windowHeight) {
-      dude.visible = false;
-      dude.interactive = false;
-    }
-
     app.stage.addChild(dude);
 
     return dude;
   });
 
-  app.stage.interactive = true;
-
-  app.stage.hitArea = app.renderer.screen;
-
-  app.stage.addListener("click", (e) => {
-    console.log("clicked", e.target);
-    if (e.target.isSprite) {
-      const { clusterId, status } = e.target._customData;
-
-      pop.innerHTML = `clusterId: ${clusterId} \n status: ${status}`;
-
-      pop.style.setProperty("top", `${e.target.y}px`);
-      pop.style.setProperty("left", `${e.target.x}px`);
-
-      pop.style.setProperty("display", "block");
-    } else {
-      pop.style.setProperty("display", "none");
-    }
-  });
-
-  function handleMouseWhell(e: WheelEvent) {
-    // console.log("whell", e.deltaY);
-
-    if (e.deltaY < 0) {
-      itemSize *= 0.95;
-      minSpace *= 0.95;
-
-      if (itemSize < 1) itemSize = 2;
-
-      if (minSpace < 1) minSpace = 1;
-    } else {
-      itemSize *= 1.05;
-      minSpace *= 1.05;
-
-      if (itemSize > 100) itemSize = 100;
-
-      if (minSpace > 5) minSpace = 5;
-    }
-
-    // console.log("item size", itemSize);
-
-    const rowNum = Math.floor(windowWidth / (itemSize + minSpace));
-
+  function render() {
+    const rowNum = Math.floor(windowWidth / (itemSize + space));
     const rowSpace = (windowWidth - rowNum * itemSize) / (rowNum - 1);
 
     dudes.forEach((dude, index) => {
-      const y = Math.floor(index / rowNum) * (itemSize + 5);
+      const y = Math.floor(index / rowNum) * (itemSize + space) + offsetY;
 
-      if (y > windowHeight) {
+      if (y > windowHeight || y < 0 - itemSize) {
         dude.visible = false;
 
         dude.interactive = false;
@@ -163,11 +123,68 @@ function main() {
     });
   }
 
+  function handleMouseWhell(e: WheelEvent) {
+    if (e.deltaY < 0) {
+      itemSize *= 0.95;
+    } else if (e.deltaY > 0) {
+      itemSize *= 1.05;
+    }
+
+    if (itemSize < 1) {
+      itemSize = 1;
+    } else if (itemSize > 100) {
+      itemSize = 100;
+    }
+
+    render();
+  }
+
   const throttleHandleMouseWhell = _.throttle(handleMouseWhell, 1000 / 30);
 
   app.view.addEventListener("wheel", (e: WheelEvent) => {
     throttleHandleMouseWhell(e);
   });
+
+  window.addEventListener("resize", () => {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    render();
+  });
+
+  // 拖动
+  app.view.addEventListener("mousedown", (e) => {
+    startY = e.clientY;
+    dragAble = true;
+  });
+
+  app.view.addEventListener("mousemove", (e) => {
+    if (!dragAble) return;
+
+    const endY = e.clientY;
+    offsetY += endY - startY;
+
+    render();
+
+    startY = endY;
+  });
+
+  app.view.addEventListener("mouseup", () => {
+    startY = 0;
+    dragAble = false;
+  });
+
+  render();
+
+  function animate() {
+    render();
+
+    offsetY -= 10;
+
+    requestAnimationFrame(animate);
+  }
+
+  // animate();
 }
 
 main();
